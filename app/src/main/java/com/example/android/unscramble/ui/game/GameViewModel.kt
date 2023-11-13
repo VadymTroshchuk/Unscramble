aaa57d2676ab86a66409dfcfd57f7fc60829a406
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-13 16:36:36 +0200

    implement getNextWord method

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,6 +5,23 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
     init {
         Log.d("GameFragment", "GameViewModel created!")
     }
@@ -13,12 +30,15 @@ class GameViewModel : ViewModel() {
     val count: Int
         get() = _count
 
-
     private var currentWordCount = 0
-
 
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 }
 
